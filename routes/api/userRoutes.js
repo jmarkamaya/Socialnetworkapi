6e01bde3d05964ed45d1b287f1f3e3800a3a2f08
Jon Amaya <jonamaya@Jons-MacBook-Air.local>
2023-09-19 17:55:25 -0700

    added date format code and user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,23 +1,17 @@
 const router = require('express').Router();
 const {
-  getStudents,
-  getSingleStudent,
-  createStudent,
-  deleteStudent,
-  addAssignment,
-  removeAssignment,
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
 } = require('../../controllers/userController');
-
-// /api/students
-router.route('/').get(getStudents).post(createStudent);
-
-// /api/students/:studentId
-router.route('/:studentId').get(getSingleStudent).delete(deleteStudent);
-
-// /api/students/:studentId/assignments
-router.route('/:studentId/assignments').post(addAssignment);
-
-// /api/students/:studentId/assignments/:assignmentId
-router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
-
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 module.exports = router;
